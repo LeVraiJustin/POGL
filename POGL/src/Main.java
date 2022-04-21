95974ab69a81c2e3cc7ad998d708ff39f6864be6
Justin <gussm77@outlook.fr>
2022-04-21 21:54:25 +0200

    Initialisation du projet

---
+++ POGL/src/Main.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+
+public class Main {
+
+    // Dans cette classe on va mettre toute les déclarations
+    // Ca va être ici qu'on va lancer le jeu
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            CModele modele = new CModele();
+            CVue vue = new CVue(modele);
+        });
+
+    }
+
+}
+
