5be05a4d163f2acd25b3f78cb4333d3b22abee88
Justin <gussm77@outlook.fr>
2022-04-22 23:33:14 +0200

    update 0.2

---
+++ POGL/src/Artefact.java
@@ -0,0 +1,10 @@
+/**
+ * Type enum pour les artefacts
+ * On a donc ici les 4 artefacts à récupérer
+ * + NONE qui siginifie qu'il n'y a pas d'artéfact.
+ */
+
+public enum Artefact {
+    CRYSTAL, STATUE,
+    CALICE, PIERRE, NONE
+}
